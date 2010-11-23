e6d80d11c834e66b45019bd562aceaa52913ef27
qieqie.wang <qieqie.wang@88edfdb4-03b2-670d-8de4-81cde103057c>
2010-11-23 16:22:40 +0000

    check in 3

--- trunk/java-project/common/src/main/java/net/paoding/spdy/common/frame/PingExecution.java
+++ trunk/java-project/common/src/main/java/net/paoding/spdy/common/frame/PingExecution.java
@@ -1,9 +1,11 @@
 package net.paoding.spdy.common.frame;
 
-import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
 
 import net.paoding.spdy.common.frame.frames.Ping;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.jboss.netty.channel.ChannelHandler.Sharable;
 import org.jboss.netty.channel.ChannelHandlerContext;
 import org.jboss.netty.channel.Channels;
@@ -13,20 +15,23 @@ import org.jboss.netty.channel.SimpleChannelHandler;
 /**
  * ping处理器
  * <p>
+ * 
  * @author qieqie.wang@gmail.com
  * 
  */
 @Sharable
 public class PingExecution extends SimpleChannelHandler {
 
+    private static Log logger = LogFactory.getLog(PingExecution.class);
+
     private PingListener pingListener;
 
-    private Executor executor;
+    private ExecutorService executor;
 
     public PingExecution() {
     }
 
-    public PingExecution(Executor executor, PingListener pingListener) {
+    public PingExecution(ExecutorService executor, PingListener pingListener) {
         setExecutor(executor);
         setPingListener(pingListener);
     }
@@ -35,7 +40,7 @@ public class PingExecution extends SimpleChannelHandler {
         this.pingListener = pingListener;
     }
 
-    public void setExecutor(Executor executor) {
+    public void setExecutor(ExecutorService executor) {
         this.executor = executor;
     }
 
@@ -44,11 +49,18 @@ public class PingExecution extends SimpleChannelHandler {
         Object msg = e.getMessage();
         if (msg instanceof Ping) {
             final Ping ping = (Ping) msg;
-            //System.out.println("ping received: " + e.getChannel().getRemoteAddress() + " " + ping.getId());
             if (ping.getId() % 2 == 1) {
                 // Receivers of a PING frame should send an identical frame 
                 // to the sender as soon as possible echo
                 Channels.write(e.getChannel(), ping);
+                if (logger.isInfoEnabled()) {
+                    logger.info("ping echo to '" + e.getChannel().getRemoteAddress() + "' " + ping);
+                }
+            } else {
+                if (logger.isInfoEnabled()) {
+                    logger.info("ping responsed from '" + e.getChannel().getRemoteAddress() + "' "
+                            + ping);
+                }
             }
             if (pingListener != null) {
                 if (executor == null) {
@@ -61,6 +73,10 @@ public class PingExecution extends SimpleChannelHandler {
                             pingListener.pingArrived(ping);
                         }
                     });
+                }
+            } else {
+                if (logger.isDebugEnabled()) {
+                    logger.debug("there's not pingListener for ping arriving:" + ping);
                 }
             }
         } else {
